7e2a6f52463d263e9ce0f91e82b3ba040a980775
zhl146 <zhl146@gmail.com>
2022-05-30 20:22:57 +0000

    feat: start in bash term, fix ctrl-c not working

--- torch/start.sh
+++ torch/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "pod started"
+
 if [[ $PUBLIC_KEY ]]
 then
     mkdir -p ~/.ssh
@@ -14,9 +16,7 @@ fi
 if [[ $JUPYTER_PASSWORD ]]
 then
     cd /
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+else
+    sleep infinity
 fi
-
-echo "pod started"
-
-sleep infinity
