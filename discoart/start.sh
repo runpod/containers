d40c01408456cd1a9ae5d593708bca03574112e2
zeen <zhl146@gmail.com>
2022-08-28 14:40:40 -0400

    feat: add disco and stable

---
+++ discoart/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "Container Started"
+
+cd /
+wget https://www.feverdreams.app/models.yaml
+
+# Use FD model list
+export DISCOART_MODELS_YAML='/models.yaml'
+
+# Start service
+cd /discoart-ui
+yarn start &
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+    echo "SSH Service Started"
+fi
+
+if [[ $JUPYTER_PASSWORD ]]
+then
+    ln -sf /examples /workspace
+    ln -sf /root/welcome.ipynb /workspace
+
+    cd /
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* \
+        --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' \
+        --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+    echo "Jupyter Lab Started"
+fi
+
+sleep infinity
