158ea435a6a0e054ec7c99d7a346db41e9fb0fc9
Ubuntu <ubuntu@ip-172-31-1-123.ec2.internal>
2022-12-01 13:44:44 +0000

    add sd

---
+++ stablediffusion-v1/relauncher.py
@@ -0,0 +1,15 @@
+import os, time
+
+n = 0
+gradio_auth = os.getenv('GRADIO_AUTH')
+while True:
+    print('Relauncher: Launching...')
+    if n > 0:
+        print(f'\tRelaunch count: {n}')
+    launch_string = "python webui.py --port 3000 --xformers --ckpt /workspace/stable-diffusion-webui/v1-5-pruned-emaonly.ckpt --opt-split-attention --listen --enable-insecure-extension-access"
+    if gradio_auth:
+        launch_string += " --gradio-auth " + gradio_auth
+    os.system(launch_string)
+    print('Relauncher: Process is ending. Relaunching in 2s...')
+    n += 1
+    time.sleep(2)
