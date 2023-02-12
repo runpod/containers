71f6e0b81b1cc3210727c4c5ddacf79b40167d39
zhl146 <zhl146@gmail.com>
2023-02-12 17:01:00 +0000

    fix example

--- serverless-automatic/start.sh
+++ serverless-automatic/start.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=1
 source /workspace/venv/bin/activate
 cd /workspace/stable-diffusion-webui
 echo "starting api"
-python webui.py --port 3000 --nowebui --api --xformers --ckpt /workspace/v1-5-pruned-emaonly.ckpt &
+python webui.py --port 3000 --nowebui --api --xformers --ckpt /model.safetensors &
 cd /
 
 echo "starting worker"
