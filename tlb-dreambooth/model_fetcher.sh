5ba4cdc152169ade1df7ef9023eed816e79a0633
Justin Merrell <merrelljustin@gmail.com>
2023-02-19 21:23:14 -0500

    Update model_fetcher.sh

--- tlb-dreambooth/model_fetcher.sh
+++ tlb-dreambooth/model_fetcher.sh
@@ -3,8 +3,8 @@ cd /src/stable-diffusion-v1-5
 git init
 git lfs install --system --skip-repo
 git remote add -f origin  "https://huggingface.co/runwayml/stable-diffusion-v1-5"
-# git config core.sparsecheckout true
-# echo -e "scheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json\n!vae/diffusion_pytorch_model.bin\n!*.safetensors" > .git/info/sparse-checkout
+git config core.sparsecheckout true
+echo -e "\nscheduler\ntext_encoder\ntokenizer\nunet\nvae\nmodel_index.json\n!vae/diffusion_pytorch_model.bin\n!*.safetensors" > .git/info/sparse-checkout
 git pull origin main
 wget -q -O vae/diffusion_pytorch_model.bin https://huggingface.co/stabilityai/sd-vae-ft-mse/resolve/main/diffusion_pytorch_model.bin
 rm -r .git
