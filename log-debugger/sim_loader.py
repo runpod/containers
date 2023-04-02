4ad6110d4460fcbeff5dc8c68d586189d133fcc6
Justin Merrell <merrelljustin@gmail.com>
2023-04-02 12:32:52 -0400

    Update sim_loader.py

--- log-debugger/sim_loader.py
+++ log-debugger/sim_loader.py
@@ -4,6 +4,15 @@ Runs a long running loading bar to cause container log to hang.
 
 from tqdm import tqdm
 
+# -------------------------------- Print Test -------------------------------- #
+print("Test 1001 single line prints")
+
+for index, _ in enumerate(range(1000000000)):
+    print(f"Test print {index}")
+
+# --------------------------------- tqdm Test -------------------------------- #
+print("Test tqdm bar")
+
 for iter in tqdm(range(1000000000)):
     pass
 
