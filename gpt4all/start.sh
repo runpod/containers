d1ff0591bc194f0a2e75fb3e9d37271083c534a0
zhl146 <zhl146@gmail.com>
2023-04-18 01:30:27 +0000

    move gradio ui start out of jupyter start if statement

--- gpt4all/start.sh
+++ gpt4all/start.sh
@@ -2,6 +2,10 @@
 
 echo "pod started"
 
+cd /src/gpt4all-ui
+source /src/gpt4all-ui/env/bin/activate
+python3 app.py --model "gpt4all-lora-quantized-ggml.bin" --port 9600 --host "0.0.0.0" &
+
 if [[ $PUBLIC_KEY ]]
 then
     mkdir -p ~/.ssh
@@ -15,10 +19,6 @@ fi
 
 if [[ $JUPYTER_PASSWORD ]]
 then
-    cd /src/gpt4all-ui
-    source /src/gpt4all-ui/env/bin/activate
-    python3 app.py --model "gpt4all-lora-quantized-ggml.bin" --port 9600 --host "0.0.0.0" &
-
     cd /
     jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
 else
