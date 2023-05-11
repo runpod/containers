52e6689caced4cbcf526ec376fc541294f27e737
Justin Merrell <merrelljustin@gmail.com>
2023-05-10 23:08:06 -0400

    feat: added a github runner container

---
+++ github-runner/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+GH_OWNER=$GH_OWNER
+GH_REPOSITORY=$GH_REPOSITORY
+GH_TOKEN=$GH_TOKEN
+
+RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner
+
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
