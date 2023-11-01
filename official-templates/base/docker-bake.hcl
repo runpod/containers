d79a9cf50fcdd24d7e4efb18734be7bc7fde5925
Justin Merrell <merrelljustin@gmail.com>
2023-11-01 00:17:11 -0400

    feat: auto shutdown and cuda tag

--- official-templates/base/docker-bake.hcl
+++ official-templates/base/docker-bake.hcl
@@ -1,13 +1,16 @@
 variable "RELEASE" {
-    default = "0.2.2"
+    default = "0.3.0"
 }
 
 target "default" {
     dockerfile = "Dockerfile"
-    tags = ["runpod/base:${RELEASE}"]
+    tags = ["runpod/base:${RELEASE}-cuda11.1.1"]
     contexts = {
         scripts = "../../container-template"
         proxy = "../../container-template/proxy"
         logo = "../../container-template"
+    }
+    args = {
+        BASE_IMAGE = "nvidia/cuda:11.1.1-devel-ubuntu20.04"
     }
 }
---
+++ official-templates/base/post_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z "${RUNPOD_PROJECT_ID}" ]]; then
+    echo "RUNPOD_PROJECT_ID environment variable is not set. Exiting."
+    exit 0
+fi
+
+interval=60
+
+# Function to monitor the number of active SSH connections
+monitor_ssh() {
+    while true; do
+        # Using 'ss' to count active SSH connections
+        connections=$(ss -tn | grep ':22' | wc -l)
+        if [[ "$connections" -gt 0 ]]; then
+            echo "ssh connections active"
+        else
+            echo "no active ssh connections"
+            runpodctl remove pod $RUNPOD_POD_ID
+        fi
+        sleep $interval
+    done
+}
+
+monitor_ssh &
