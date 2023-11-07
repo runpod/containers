50fce7767c0973cd7433d64ac7a38e332b29e9af
Justin Merrell <merrelljustin@gmail.com>
2023-11-07 18:01:32 -0500

    Update start.sh

--- container-template/start.sh
+++ container-template/start.sh
@@ -30,19 +30,19 @@ setup_ssh() {
         chmod 700 -R ~/.ssh
 
          if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-            ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
+            ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
         fi
 
         if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
-            ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
+            ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
         fi
 
         if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
-            ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
+            ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
         fi
 
         if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
-            ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+            ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
         fi
 
         service ssh start
