dce676cd666cf1fcaf4a3b754dabe70c47cb8a9c
Justin Merrell <merrelljustin@gmail.com>
2023-11-07 11:10:58 -0500

    Update post_start.sh

--- official-templates/base/post_start.sh
+++ official-templates/base/post_start.sh
@@ -5,21 +5,34 @@ if [[ -z "${RUNPOD_PROJECT_ID}" ]]; then
     exit 0
 fi
 
-interval=${POD_INACTIVITY_TIMEOUT:-60}
+check_interval=60
+countdown_time=${POD_INACTIVITY_TIMEOUT:-300}
 
 # Function to monitor the number of active SSH connections
 monitor_ssh() {
-    echo "Monitoring SSH connections every $interval seconds"
+    echo "Monitoring SSH connections every $check_interval seconds"
+    countdown=$countdown_time
 
     while true; do
-        sleep $interval
+        sleep $check_interval
 
-        connections=$(ss -tn | grep ':22' | wc -l)
+        connections=$(ss -tn | grep ':22' | grep -v 'LISTEN' | wc -l)
 
         if [[ "$connections" -eq 0 ]]; then
-            runpodctl remove pod $RUNPOD_POD_ID
-        fi
+            ((countdown-=$check_interval))
+            echo "No SSH connections found. Countdown: $countdown seconds remaining."
 
+            if [[ "$countdown" -le 0 ]]; then
+                echo "Countdown reached zero. Removing pod: $RUNPOD_POD_ID"
+                runpodctl remove pod $RUNPOD_POD_ID
+                exit 0
+            fi
+        else
+            if [[ "$countdown" -ne $countdown_time ]]; then
+                echo "SSH connection detected. Countdown aborted."
+            fi
+            countdown=$countdown_time
+        fi
     done
 }
 
