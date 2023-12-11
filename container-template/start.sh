79aca4c91500566b0af847accaa2979ac21784da
Justin Merrell <merrelljustin@gmail.com>
2023-12-11 08:53:14 -0500

    feat: print ssh key fingerprint

--- container-template/start.sh
+++ container-template/start.sh
@@ -31,24 +31,35 @@ setup_ssh() {
 
          if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
             ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
+            echo "RSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_rsa_key.pub
         fi
 
         if [ ! -f /etc/ssh/ssh_host_dsa_key ]; then
             ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ''
+            echo "DSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_dsa_key.pub
         fi
 
         if [ ! -f /etc/ssh/ssh_host_ecdsa_key ]; then
             ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ''
+            echo "ECDSA key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_ecdsa_key.pub
         fi
 
         if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
             ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -q -N ''
+            echo "ED25519 key fingerprint:"
+            ssh-keygen -lf /etc/ssh/ssh_host_ed25519_key.pub
         fi
 
         service ssh start
 
         echo "SSH host keys:"
-        cat /etc/ssh/*.pub
+        for key in /etc/ssh/*.pub; do
+            echo "Key: $key"
+            ssh-keygen -lf $key
+        done
     fi
 }
 
