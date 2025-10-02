d4638b0e3fde2a2f7f7ccf2d1e5546563c085200
Dj Isaac <contact@dejaydev.com>
2025-10-01 21:41:01 -0500

    fix: pip version mismatch breaking venvs

--- container-template/start.sh
+++ container-template/start.sh
@@ -78,7 +78,7 @@ start_jupyter() {
         echo "Starting Jupyter Lab..."
         mkdir -p /workspace &&
             cd / &&
-            nohup python3.10 -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
+            nohup python3 -m jupyter lab --allow-root --no-browser --port=8888 --ip=* --FileContentsManager.delete_to_trash=False --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --IdentityProvider.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &> /jupyter.log &
         echo "Jupyter Lab started"
     fi
 }
